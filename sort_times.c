5e61f440d9c081c4e215442e5bdde3383cdaa6a0
Rocky Whitely <rocky.whitely@blinn.edu>
2023-12-15 10:27:07 -0600

    Added comments to recursive mergesort
    Added additional comments to code

--- sort_times.c
+++ sort_times.c
@@ -18,7 +18,7 @@
 void merge_sort(int arr[], int left, int right);
 void merge(int arr[], int left, int mid, int right);
 int compare(const void *x, const void *y);
-void print_results(int arr[], long *start_time, long *end_time, char name[]);
+void print_results(const int arr[], const long *start_time, const long *end_time, char name[]);
 
 const size_t count = 32767;
 
@@ -26,7 +26,7 @@ int main(int argc, char const *argv[])
 {
     long start_time, end_time;
     double elapsed;
-    // declare arr[ay]
+    // declare arr[ays]
     // initialize with random numbers
     int arr[count];
     int arrB[count];
@@ -42,16 +42,19 @@ int main(int argc, char const *argv[])
         arrC[i] = num;
     }
 
+    // Time 'qsort'
     start_time = clock();
     qsort(arrC, count, sizeof(int), compare);
     end_time = clock();
     print_results(arrC, &start_time, &end_time, "QSORT in stdlib.h\t\t  ");
 
+    // Time 'mergesort'
     start_time = clock();
     int result = mergesort(arrB, count, sizeof(int), compare);
     end_time = clock();
     print_results(arrB, &start_time, &end_time, "MERGESORT in stdlib.h\t\t  ");
 
+    // Time 'recursive merge_sort'
     start_time = clock();
     merge_sort(arr, 0, (int)count - 1);
     end_time = clock();
@@ -60,12 +63,14 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
+// divide-and-conquer merge_sort
 void merge_sort(int arr[], int left, int right)
 {
     // repeatively halve array into smaller groups, eventually into individual components
     if (right - left > 0)
     {
         int mid = left + (right - left) / 2;
+        // recursively split array in halves
         merge_sort(arr, left, mid);
         merge_sort(arr, mid + 1, right);
         // merge groups
@@ -73,6 +78,7 @@ void merge_sort(int arr[], int left, int right)
     }
 }
 
+// sort & merge adjacent slices of array
 void merge(int arr[], int left, int mid, int right)
 {
     // sort increasingly larger slices of array
@@ -82,12 +88,16 @@ void merge(int arr[], int left, int mid, int right)
     if (arr[midRight] < arr[left])
     {
         int temp = arr[midRight];
+        // move array slice right by one place
         memcpy(arr + left + 1, arr + left, (midRight - left) * sizeof(int));
+        // replace left element with new left
         arr[left] = temp;
+        // recursively merge remainder of array slice
         merge(arr, left + 1, midRight, right);
     }
     else
     {
+        // recursively merge remainder of array slice
         merge(arr, left + 1, mid, right);
     }
 }
@@ -97,12 +107,13 @@ int compare(const void *x, const void *y)
     return (*(int *)(x) - *(int *)(y));
 }
 
-void print_results(int arr[], long *start_time, long *end_time, char name[])
+void print_results(const int arr[], const long *start_time, const long *end_time, char name[])
 {
+    // calculate elapsed time in milliseconds
     double elapsed = (*end_time - *start_time) * 1000.0 / CLOCKS_PER_SEC;
     printf("%s:\t%6.3f ms\n", name, elapsed);
 
-    // test that array is sorted
+    // confirm that array is sorted
     int last = -1;
     for (size_t i = 0; i < count; i++)
     {
@@ -115,6 +126,7 @@ void print_results(int arr[], long *start_time, long *end_time, char name[])
     }
     printf("All elements in sorted order: #%zu = %d\n", count, arr[count - 1]);
 
+    // visual check of last 10 elements of array for comparison with other sort routines
     printf("Last 10 elements: ");
     for (size_t i = count - 10; i < count; i++)
     {
