6bfeef8a537573fb2503830250d918d9ff1f629f
Rocky Whitely <rocky.whitely@blinn.edu>
2023-12-17 22:51:52 -0600

    merge_sort.c sorting with 'temp' array

--- merge_sort.c
+++ merge_sort.c
@@ -13,7 +13,7 @@
 void mergeSort(int arr[], int left, int right);
 void merge(int arr[], int left, int mid, int right);
 
-const size_t count = 250;
+const size_t count = 32767;
 
 int main(int argc, char const *argv[])
 {
@@ -21,17 +21,14 @@ int main(int argc, char const *argv[])
     // initialize with random numbers
     int arr[count];
     srand(time(0));
-    for (size_t i = 0; i < count; i++)
-    {
-        arr[i] = rand() % 100;
-    }
 
     printf("Original array: ");
     for (size_t i = 0; i < count; i++)
     {
+        arr[i] = rand() % 32767;
         printf("%d ", arr[i]);
     }
-    printf("\n");
+    printf("\n\n");
 
     // run mergesort
     mergeSort(arr, 0, (int)count - 1);
@@ -54,7 +51,7 @@ int main(int argc, char const *argv[])
         }
         last = arr[i];
     }
-    printf("All elements in sorted order\n");
+    printf("All elements in sorted order: #%zu = %d\n", sizeof(arr)/sizeof(arr[0]), arr[count - 1]);
 
     return 0;
 }
@@ -72,21 +69,74 @@ void mergeSort(int arr[], int left, int right)
     }
 }
 
+// sort & merge adjacent slices of array to 'temp' array
 void merge(int arr[], int left, int mid, int right)
 {
-    // sort increasingly larger slices of array
-    int midRight = mid + 1;
-    if (left > mid || midRight > right)
-        return;
-    if (arr[midRight] < arr[left])
+    int temp[count];
+    int midR = mid + 1;
+    int tLeft = left;
+    int aLeft = left;
+
+    // sort lefthand & righthand of array to 'temp' array
+    // one element at a time; Break when one hand is sorted
+    while (left <= mid && midR <= right)
     {
-        int temp = arr[midRight];
-        memcpy(arr + left + 1, arr + left, (midRight - left) * sizeof(int));
-        arr[left] = temp;
-        merge(arr, left + 1, midRight, right);
+        if (arr[left] <= arr[midR])
+        {
+            temp[tLeft] = arr[left];
+            left++;
+        }
+        else
+        {
+            temp[tLeft] = arr[midR];
+            midR++;
+        }
+        tLeft++;
     }
+    // Combine remainder of OLD-SORTED hand
+    // ...whether righthand
+    if (left > mid)
+    {
+        while (midR <= right)
+        {
+            temp[tLeft] = arr[midR];
+            midR++;
+            tLeft++;
+        }
+    }
+    // ...or lefthand
     else
     {
-        merge(arr, left + 1, mid, right);
+        while (left <= mid)
+        {
+            temp[tLeft] = arr[left];
+            left++;
+            tLeft++;
+        }
+    }
+    // copy 'temp' array to 'arr'
+    for (int i = aLeft; i <= right; i++)
+    {
+        arr[i] = temp[i];
     }
 }
+
+// OLD-SLOW merge method using inplace sorting
+// void merge(int arr[], int left, int mid, int right)
+// {
+//     // sort increasingly larger slices of array
+//     int midRight = mid + 1;
+//     if (left > mid || midRight > right)
+//         return;
+//     if (arr[midRight] < arr[left])
+//     {
+//         int temp = arr[midRight];
+//         memcpy(arr + left + 1, arr + left, (midRight - left) * sizeof(int));
+//         arr[left] = temp;
+//         merge(arr, left + 1, midRight, right);
+//     }
+//     else
+//     {
+//         merge(arr, left + 1, mid, right);
+//     }
+// }
