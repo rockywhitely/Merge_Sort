7c2d69937eba721794d81a462609f70d587bc5ea
Rocky Whitely <rocky.whitely@blinn.edu>
2023-12-23 13:47:21 -0600

    Added counting sort

--- sort_times.c
+++ sort_times.c
@@ -7,7 +7,20 @@
     Algorithms, (New York: McGraw_Hill Higher Education, 2008),
     section 2.3 "Mergesort", pages 50-51
 
-    mergesort & qsort (stdlib.h)
+    mergesort (mac OS) & qsort (stdlib.h)
+
+    mergesort(3) - Linux man page (https://linux.die.net/man/3/mergesort)
+    Name
+    heapsort, mergesort - sort functions
+
+    Library
+    Utility functions from BSD systems (libbsd, -lbsd)
+
+    Synopsis
+    #include <bsd/stdlib.h>
+
+    int heapsort(void *base, size_t nmemb, size_t size,int (*compar)(const void *, const void *));
+    int mergesort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
 */
 
 #include <stdio.h>
@@ -17,6 +30,7 @@
 
 void merge_sort(int arr[], int left, int right);
 void merge(int arr[], int left, int mid, int right);
+void countingSort(int input[], int output[], int length);
 int compare(const void *x, const void *y);
 void print_results(const int arr[], const long *start_time, const long *end_time, char name[]);
 
@@ -33,6 +47,7 @@ int main(int argc, char const *argv[])
     int arrB[count];
 #endif
     int arrC[count];
+    int arrD[count];
     srand(time(0));
 
     int num;
@@ -44,13 +59,14 @@ int main(int argc, char const *argv[])
         arrB[i] = num;
 #endif
         arrC[i] = num;
+        arrD[i] = num;
     }
 
-    // Time 'qsort'
+    // Time 'recursive merge_sort'
     start_time = clock();
-    qsort(arrC, count, sizeof(int), compare);
+    merge_sort(arr, 0, (int)count - 1);
     end_time = clock();
-    print_results(arrC, &start_time, &end_time, "QSORT in stdlib.h\t\t  ");
+    print_results(arr, &start_time, &end_time, "Recursive MERGESORT (temp array)  ");
 
 #ifdef __APPLE__
     // Time 'mergesort'
@@ -67,11 +83,19 @@ int main(int argc, char const *argv[])
         return 1;
     }
 #endif
-    // Time 'recursive merge_sort'
+
+    // Time 'qsort'
     start_time = clock();
-    merge_sort(arr, 0, (int)count - 1);
+    qsort(arrC, count, sizeof(int), compare);
     end_time = clock();
-    print_results(arr, &start_time, &end_time, "Recursive MERGESORT (temp array)  ");
+    print_results(arrC, &start_time, &end_time, "QSORT in stdlib.h\t\t  ");
+
+    // Time 'counting sort'
+    int output[count];
+    start_time = clock();
+    countingSort(arrD, output, (int)count);
+    end_time = clock();
+    print_results(output, &start_time, &end_time, "COUNTING SORT                     ");
 
     return 0;
 }
@@ -198,4 +222,44 @@ void print_results(const int arr[], const long *start_time, const long *end_time
         printf("%d ", arr[i]);
     }
     printf("\n\n");
+}
+// Counting Sort
+void countingSort(int input[], int output[], int length)
+{
+    // calculate maximum value
+    int k = -1;
+    for (int i = 0; i < length; i++)
+    {
+        if (k < input[i])
+            k = input[i];
+    }
+
+    // initialize count array
+    int *count = malloc((k + 1) * sizeof(int));
+    for (int i = 0; i < k+1; i++)
+    {
+        count[i] = 0;
+    }
+
+    // count values in count array
+    for (int i = 0; i < length; i++)
+    {
+        count[input[i]]++;
+    }
+
+    // accumulate count array counts
+    for (int i = 1; i < k+1; i++)
+    {
+        count[i] += count[i - 1];
+    }
+
+    // sort values by reversing input array
+    for (int i = length - 1; i >= 0; i--)
+    {
+        int j = input[i];
+        count[j]--;
+        output[count[j]] = input[i];
+    }
+
+    free(count);
 }
